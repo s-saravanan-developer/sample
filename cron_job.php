4c0e49d80b573014caaeb87a05ea3250014b951a
Saravanaja <40134718+saravanaja@users.noreply.github.com>
2019-09-14 18:33:31 +0530

    Add files via upload

---
+++ cron_job.php
@@ -0,0 +1,17 @@
+Method 1 :
+curl http://betcoins.live/betcoins/Cron/testing1
+
+Method 2 :
+curl http://betcoins.live/betcoins/Cron/testing > /dev/null 2/&1
+
+Method 3 :
+curl --silent https://betcoins.live/betcoins/Cron/testing
+
+Method 4 :
+curl --silent https://betcoins.live/betcoins/Cron/testing > /dev/null 2/&1
+
+Method 5 :
+curl --silent "www.betcoins.live/betcoins/Cron/testing"
+
+Method 6 :
+curl --silent "www.betcoins.live/betcoins/Cron/testing" > /dev/null 2/&1
